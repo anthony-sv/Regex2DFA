3497ac7e19f7269b406c44520277ccd9c7eaf7d1
anthony-sv <anthonysteiner96@gmail.com>
2021-06-07 00:05:14 -0500

    pos & anulable

--- Tree.py
+++ Tree.py
@@ -53,6 +53,7 @@ class Tree:
                     tempN = Node(data=x)
                     tempT = Tree(n=tempN)
                     tempT.setLeft(p)
+                    #tempT.setRight(Tree(n=Node()))
                     s.push(tempT)
         return s.peek()
 
@@ -64,3 +65,80 @@ class Tree:
             res.append(root.__node.getData())
             res = res + Tree.inorderTraversal(root.__right)
         return res
+
+    @staticmethod
+    def postorderTraversal(root):
+        res = []
+        if root:
+            res = Tree.postorderTraversal(root.__left)
+            res = res + Tree.postorderTraversal(root.__right)
+            res.append(root.__node.getData())
+        return res
+
+    @staticmethod
+    def postorderTraversalTree(root):
+        res = []
+        if root:
+            res = Tree.postorderTraversalTree(root.__left)
+            res = res + Tree.postorderTraversalTree(root.__right)
+            res.append(root)
+        return res
+
+    @staticmethod
+    def postorderTraversalPos(root):
+        res = []
+        if root:
+            res = Tree.postorderTraversalPos(root.__left)
+            res = res + Tree.postorderTraversalPos(root.__right)
+            res.append(root.getNode().getPos())
+        return res
+    
+    @staticmethod
+    def postorderTraversalAnul(root):
+        res = []
+        if root:
+            res = Tree.postorderTraversalAnul(root.__left)
+            res = res + Tree.postorderTraversalAnul(root.__right)
+            res.append(root.getNode().getAnulable())
+        return res
+    
+    @staticmethod
+    def tryPos(root):
+        res = Tree.postorderTraversalTree(root)
+        count = 1
+        for x in res:
+            if Symbol.isOperand(x.getNode().getData()) or x.getNode().getData() == 'ε':
+                x.getNode().setPos(count)
+                # if x.getNode().getData() == "#":
+                #     x.getNode().setData('$')
+                count = count+1
+        gg = Tree.postorderTraversalPos(root)
+        return gg
+    
+    @staticmethod
+    def tryAnulable(root):
+        res = Tree.postorderTraversalTree(root)
+        for x in res:
+            if x.getNode().getData() == 'ε':
+                x.getNode().setAnulable(True)
+            elif x.getNode().getPos() != None:
+                x.getNode().setAnulable(False)
+            elif x.getNode().getData() == '|':
+                op = x.getLeft().getNode().getAnulable() or x.getRight().getNode().getAnulable()
+                x.getNode().setAnulable(op)
+            elif x.getNode().getData() == '.':
+                op = x.getLeft().getNode().getAnulable() and x.getRight().getNode().getAnulable()
+                x.getNode().setAnulable(op)
+            elif x.getNode().getData() == '*':
+                x.getNode().setAnulable(True)
+        gg = Tree.postorderTraversalAnul(root) #reconstruct the tree
+        return gg
+    
+    # @staticmethod
+    # def depth_first_search_recursive(graph, start, visited=None):
+    #     if visited is None:
+    #         visited = set()
+    #     visited.add(start)
+    #     for next in graph[start] - visited:
+    #         Tree.depth_first_search_recursive(graph, next, visited)
+    #     return visited
--- try.py
+++ try.py
@@ -3,4 +3,8 @@ from Postfix import Postfix
 from Tree import Tree
 pfr = Postfix(Regex("(a|b)*abb"))
 tr = Tree.getSyntaxTree(pfr.getPostfix())
-print(Tree.inorderTraversal(tr))
+# print(Tree.inorderTraversal(tr))
+print(Tree.tryPos(tr))
+print(Tree.tryAnulable(tr))
+print(Tree.postorderTraversal(tr))
+print(tr.getLeft().getNode().getAnulable())
